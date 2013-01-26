73a67c7522aa46cba88eea8f07e06430f69be9f2
Guillermo Ramos Leal <guillermorl@gmail.com>
2013-01-26 02:41:13 -0800

    Target follow

---
+++ Assets/Dev/Guillr/LevelObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelObject : MonoBehaviour {
+	
+	Vector2[] wayPoints = { new Vector2(25.00f,11.00f), new Vector2(25.00f,18.00f),new Vector2(5.00f, 18.00f), new Vector2(5.00f,11.00f)};
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Dev/Guillr/MonitoPath.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class MonitoPath : MonoBehaviour {
+
+	public GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate();
+		Vector3 vec = this.transform.position;
+		vec.x -= (float)((0.8 * Time.deltaTime));
+		this.transform.position = vec;
+	}
+}
